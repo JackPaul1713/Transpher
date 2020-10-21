34f94df8404fd39784bf2b4d3a7d9698f04ed995
JackPaul1713 <JackPaul1713@gmail.com>
2020-10-20 22:33:08 -0500

    Git init using cmd I really don't know how to use pycharm for that, or if you even can... I like this better anyway. Uh 'mapping.py' contains file mapping and is pretty much done, and 'changes.py' finds the changes and conflicts and makes those changes to the file system. 'change .py' is not done yet.

---
+++ changes.py
@@ -0,0 +1,24 @@
+# Program Name: changes
+# Description: Can find changes to a mpath using a saved version of the mpath before changes.
+
+#init#
+class Change():
+    def __init__(self, type, src, dest):
+        self.type = type
+        self.src = src
+        self.dest = dest
+
+def get_changes(old, new):
+    return
+def get_conflicts(old, new):
+    return
+
+def make_changes(src_root, dest_root):
+    return
+def make_trans_changes_u():
+    return
+def make_trans_changes_d():
+    return
+
+# Author: Jack Paul Martin
+# Start: 10/20/2020, Completion:
--- mapping.py
+++ mapping.py
@@ -1,113 +1,169 @@
 # Program Name: mapping
-# Description:
-# Notes: Keep .mdir file containing versions of file maps inside main dir
+# Description: Maps out a path and records it. Uses created date as a unique id.
+# Notes: .mpath files should be for containing string versions of mapped paths
 
 #init#
 import os
 from ctypes import windll, wintypes, byref
 
-class MappedFile():
-    def __init__(self, path):
-        self.path = path
-        self.name = self.path.split('\\')[-1]
-        self.ctime = os.path.getctime(path)
-        self.mtime = os.path.getmtime(path)
-
-class MappedDir():
+class MappedPath():
     def __init__(self, init):
-        if os.path.exists(os.path.dirname(init)): # if init is a directory
+        if 'C:\\' in init and '<' not in init: # if init is a directory
+            #init#
             path = init
+            #construct#
             self.path = path
             self.name = path.split('\\')[-1]
             self.ctime = os.path.getctime(path)
-            self.sub_dir = [MappedDir(d.path) for d in os.scandir(self.path) if d.is_dir()]
-            self.files = [MappedFile(f.path) for f in os.scandir(self.path) if f.is_file()]
-            self.make_ctime_unique()
-        else:  # if init is a mapped string
-            mapped_dir_str = init
-            # take id and fill out stf
-            # separate sub_dir and files into two lists
-            # find files from the front, cutting at '|' until '<'
-            # while w != 0, where w = 1, if '<': w++, if '>' w--
-            # split at the end position of while loop
-            # make files out of id and fill files
-            # call this function for each sub_dir
+            self.mtime = os.path.getmtime(path)
+            if os.path.isdir(path):
+                self.sub_mpaths = [MappedPath(p.path) for p in os.scandir(self.path)]
+            else:
+                self.sub_mpaths = []
+        elif '<' in init: # if init is a mapped string # FIX THIS
+            #init#
+            mpath_str = init
+            def init_id_str(mpath, id_str):
+                mpath.path = id_str.split('*')[0]
+                mpath.name = mpath.path.split('\\')[-1]
+                mpath.mtime = str(id_str.split('*')[2])
+                mpath.ctime = str(id_str.split('*')[1])
+                mpath.sub_mpaths = []
+            #cut#
+            id_str = mpath_str[0:mpath_str.find('<')]
+            sub_mpaths_str = mpath_str[mpath_str.find('<')+1:len(mpath_str)-1]
+            #init_id_str, construct#
+            init_id_str(self, id_str)
+            #split_mpaths_str, construct#
+            while len(sub_mpaths_str) != 0:
+                start = sub_mpaths_str.find('<')
+                end = 0
+                i = 0
+                for c in range(start, len(sub_mpaths_str)):
+                    if sub_mpaths_str[c] == '<':
+                        i += 1
+                    elif sub_mpaths_str[c] == '>':
+                        i -= 1
+                    if i == 0:
+                        end = c
+                        break
+                if end >= start:
+                    self.sub_mpaths.append(MappedPath(sub_mpaths_str[0:end+1]))
+                sub_mpaths_str = sub_mpaths_str[end+1:len(sub_mpaths_str)]
 
     #search#
-    def search(self, mdir_find):
-        def search_recur(mdir, mdir_find):
-            if mdir.ctime == mdir_find.ctime: # search mdir
-                return(mdir)
-            for mfile in mdir.get_files(): # search mfiles
-                if mfile.ctime == mdir_find.ctime:
-                    return(mfile)
-            for msub_dir in mdir.get_sub_dirs(): # search msub_dir
-                ret = search_recur(msub_dir, mdir_find.ctime)
+    def search(self, mpath_find): # CHANGE THIS, return a position list, pos[]
+        def search_recur(mpath, mpath_find):
+            #init#
+            pos = []
+            #search mpath#
+            if mpath.ctime == mpath_find.ctime:
+                return([])
+            #search sub_mpath#
+            for i in range(len(mpath.sub_mpaths)):
+                ret = search_recur(mpath.sub_mpaths[i], mpath_find)
                 if ret != None:
-                    return(ret)
-        return(search_recur(self, mdir_find))
-    def search_dup(self, mdir_find):
-        def search_recur(mdir, mdir_find):
-            if mdir.ctime == mdir_find.ctime and mdir != mdir_find: # search mdir
-                return (mdir)
-            for mfile in mdir.get_files(): # search mfiles
-                if mfile.ctime == mdir_find.ctime and mfile != mdir_find:
-                    return (mfile)
-            for msub_dir in mdir.get_sub_dirs(): # search msub_dir
-                ret = search_recur(msub_dir, mdir_find.ctime)
+                    pos = ret
+                    pos.insert(0, i)
+                    return(pos)
+        return(search_recur(self, mpath_find))
+    def search_dup(self, mpath_find): # CHANGE THIS, return a position list, pos[]
+        def search_recur(mpath, mpath_find):
+            #init#
+            pos = []
+            #search mpath#
+            if mpath.ctime == mpath_find.ctime and mpath != mpath_find:
+                return([])
+            #search sub_mpath#
+            for i in range(len(mpath.sub_mpaths)):
+                ret = search_recur(mpath.sub_mpaths[i], mpath_find)
                 if ret != None:
-                    return (ret)
-        return(search_recur(self, mdir_find))
+                    pos = ret
+                    pos.insert(0, i)
+                    return(pos)
+        return(search_recur(self, mpath_find))
 
     #modify#
-    def make_ctime_unique(self):
-        def mod_ctime(path):
-            timestamp = int((os.path.getctime(path) * 10000000) + 116444736000000000 + 10)
+    def get_mpath_at_pos(self, pos):
+        mpath = self
+        for p in pos:
+            mpath = mpath.sub_mpaths[p]
+        return(mpath)
+    def add_mpath_at_pos(self, mpath_add, pos):
+        def sub(mpath, mpath_add, pos):
+            if len(pos) > 0:
+                mpath.sub_mpaths[pos[0]] = sub(mpath.sub_mpaths[pos[0]], mpath_add, pos[1:len(pos)])
+                return(mpath)
+            elif len(pos) == 0:
+                mpath.sub_mpaths.append(mpath_add)
+                return(mpath)
+        self.sub_mpaths = sub(self, mpath_add, pos).sub_mpaths
+    def rem_mpath_at_pos(self, pos):
+        def sub(mpath, pos):
+            print('new_pos:', pos)
+            if len(pos) > 1:
+                mpath.sub_mpaths[pos[0]] = sub(mpath.sub_mpaths[pos[0]], pos[1:len(pos)])
+                return(mpath)
+            elif len(pos) == 1:
+                mpath.sub_mpaths.pop(pos[0])
+                return(mpath)
+        self.sub_mpaths = sub(self, pos).sub_mpaths
+    def move_mpath_at_pos(self, pos_from, pos_to):
+        mpath = self.get_mpath_at_pos(pos_from)
+        self.rem_mpath_at_pos(pos_from)
+        self.add_mpath_at_pos(mpath, pos_to)
+    def update_mpath_at_pos(self, mpath, pos):
+        self.rem_mpath_at_pos(pos)
+        self.add_mpath_at_pos(mpath, pos)
+
+    def make_ctime_unique(self): # CHANGE THIS to work with updated search functions, remove refresh
+        def mod_ctime(path, mod_size):
+            timestamp = int((os.path.getctime(path) * 10000000) + 116444736000000000 + (10 * mod_size))
             ctime = wintypes.FILETIME(timestamp & 0xFFFFFFFF, timestamp >> 32)
             handle = windll.kernel32.CreateFileW(path, 256, 0, None, 3, 128, None)
             windll.kernel32.SetFileTime(handle, byref(ctime), None, None)
             windll.kernel32.CloseHandle(handle)
-        def make_ctime_unique_recur(mdir):
-            if self.search_dup(mdir) != None:
-                mod_ctime(mdir.path)
-            for mfile in self.files:
-                if self.search_dup(mfile) != None:
-                    mod_ctime(mfile.path)
-            for msub_dir in self.sub_dir:
-                make_ctime_unique_recur(msub_dir)
-        self.refresh()
+        def make_ctime_unique_recur(mpath):
+            counter = 1
+            #make ctime unique for mpath#
+            while True:
+                if self.search_dup(mpath) != None:
+                    mpath_dup_pos = self.search_dup(mpath)
+                    mpath_dup = self.get_mpath_at_pos(mpath_dup_pos)
+                    mod_ctime(mpath_dup.path, counter)
+                    self.update_mpath_at_pos(MappedPath(mpath_dup.path), mpath_dup_pos)
+                    counter += 1
+                else:
+                    break
+            #make ctime unique for sub_mpaths#
+            for sub_mpath in mpath.sub_mpaths:
+                make_ctime_unique_recur(sub_mpath)
+        make_ctime_unique_recur(self)
     def refresh(self):
-        self.ctime = os.path.getctime(self.path)
-        self.sub_dir = [MappedDir(d.path) for d in os.scandir(self.path) if d.is_dir()]
-        self.files = [MappedFile(f.path) for f in os.scandir(self.path) if f.is_file()]
+        if os.path.isdir(self.path):
+            self.sub_mpaths = [MappedPath(p.path) for p in os.scandir(self.path)]
 
     #output#
-    def get_mdir_str(self):
-        def make_id_str(dir):
-            return (self.path + '*' + str(self.ctime))
-        def get_mdir_str_recur(mdir):
-            mdir_str = make_id_str(mdir) + '<' # add dir
-            for file in mdir.get_files(): # add files
-                mdir_str = mdir_str + make_id_str(file) + '|'
-            for sub_dir in mdir.get_sub_dirs(): # add sub_dir
-                mdir_str = mdir_str + get_mdir_str_recur(sub_dir) + '|'
-            mdir_str = mdir_str + '\b' + '>'
-            return(mdir_str)
-        return(get_mdir_str_recur(self))
-    def get_changes_of(self, mapped_dir): # moves, renames -then- adds, deletes
-        # start at top dir
-        # record changes
-        # fix mapped_dir object
-        # move downward
-        # record changes
-        # fix mapped_dir object
-        return()
+    def get_mpath_str(self):
+        def make_id_str(mpath):
+            return(mpath.path + '*' + str(mpath.ctime) + '*' + str(mpath.mtime))
+        def get_mpath_str_recur(mpath):
+            #add mpath#
+            mpath_str = make_id_str(mpath) + '<'
+            #add sub_mpaths#
+            for sub_dir in mpath.sub_mpaths:
+                mpath_str = mpath_str + get_mpath_str_recur(sub_dir)
+            #close mpath#
+            mpath_str = mpath_str + '>'
+            return(mpath_str)
+        return(get_mpath_str_recur(self))
 
 #testing#
 if __name__ == '__main__':
-    print("no testing at this point")
-    # test ids next (file handles)
-    # test compare_dir() next
+    test_mpath = MappedPath('a*0*0<b*0*0<c*0*0<d*0*0<>d1*0*0<e*0*0<>>>>b1*0*0<>>')
+    print(test_mpath.get_mpath_str())
+    test_mpath.make_ctime_unique()
+    print(test_mpath.get_mpath_str())
 
 # Author: Jack Paul Martin
-#
+# Start: idk, Completion: 10/20/2020ish
