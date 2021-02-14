27d92cbcd3a5f66074c551b70e8a6e9f171416a5
JackPaul <JackPaul1713@gmail.com>
2021-02-13 20:52:58 -0600

    Organization
    organized "main.py"

--- main.py
+++ main.py
@@ -38,58 +38,85 @@ if __name__ == '__main__':
             local_m = 1
             exterior_m = 0
 
-        #check# - get any changes on the local machine
-        print('CHECK')
-        mpath = mapping.MappedPath(tfile.path[local_m], exclusions=tfile.path[local_m])  # get new mpath to compare to the old one
-        binding.make_ctimes_unique(mpath)  # make sure any new files have unique ctimes
-        new_changes = changes.get_changes(tfile.mpath[local_m], mpath)  # compare old_mpath to new_mpath
+        #CHECK# - get any changes on the local machine
+        #var#
+        old_mpath = tfile.mpath[local_m]
+        new_mpath = mapping.MappedPath(tfile.path[local_m], exclusions=tfile.path[local_m])  # get new new_mpath to compare to the old one
+        old_changes = tfile.changes(local_m)
+        new_changes = changes.empty_changes
+        merged_changes = changes.empty_changes
 
-        print('old mpath: ' + mpath.get_mpath_str())
-        print('new mpath: ' + tfile.mpath[local_m].get_mpath_str())
+        #check#
+        binding.make_ctimes_unique(new_mpath)  # make sure any new files have unique ctimes
+        new_changes = changes.get_changes(tfile.mpath[local_m], new_mpath)  # compare old_mpath to new_mpath
+        tfile.mpath[local_m] = new_mpath
+
+        if not changes.is_empty(old_changes): # if there are old changes
+            merged_changes = changes.hard_merge_changes(tfile.changes[local_m], new_changes)  # merge old_changes with new_changes
+            tfile.changes[local_m] = merged_changes  # add merged changes
+            changes.make_trans_changes(tfile.changes[local_m], tfile.tpath, tfile.mpath[local_m])  # make transitional changes so changes can be made
+        elif not changes.is_empty(new_changes): # if there are no old changes but there are new ones
+            tfile.changes[local_m] = new_changes # add changes
+            changes.make_trans_changes(tfile.changes[local_m], tfile.tpath, tfile.mpath[local_m])  # make transitional changes so changes can be made
+
+        #display#
+        print('CHECK')
+        print('old new_mpath: ' + new_mpath.get_mpath_str())
+        print('new new_mpath: ' + old_mpath.get_mpath_str())
         print('')
         print('old changes(local):')
-        for change in tfile.changes[local_m]:
+        for change in old_changes:
             change.display()
         print('')
         print('new changes(local):')
         for change in new_changes:
             change.display()
         print('')
-
-        tfile.mpath[local_m] = mpath
-        if not changes.is_empty(tfile.changes[local_m]): # if there are old changes
-            tfile.changes[local_m] = changes.hard_merge_changes(tfile.changes[local_m], new_changes)  # add merged changes, merge old_changes with new_changes
-
+        if not changes.is_empty(old_changes):  # if there are old changes
             print('merged changes(local):')
             for change in new_changes:
                 change.display()
             print('')
 
-            changes.make_trans_changes(tfile.changes[local_m], tfile.tpath, tfile.mpath[local_m])  # make transitional changes so changes can be made
-        elif not changes.is_empty(new_changes): # if there are no old changes but there are new ones
-            tfile.changes[local_m] = new_changes # add changes
-            changes.make_trans_changes(tfile.changes[local_m], tfile.tpath, tfile.mpath[local_m])  # make transitional changes so changes can be made
+        #APPLY# - merge any changes on the local machine with changes on the exterior machine and apply merged exterior changes(staged changes)
+        #var#
+        local_changes = tfile.changes(local_m)
+        exterior_changes = tfile.changes(exterior_m)
+        merged_local_changes = changes.empty_changes
+        staged_changes = changes.empty_changes
 
-        #apply# - merge any changes on the local machine with changes on the exterior machine and apply merged exterior changes(staged changes)
+        #apply#
+        if (not changes.is_empty(local_changes)) and (not changes.is_empty(exterior_changes)):  # if there are changes on the local and exterior machine
+            merged_local_changes, staged_changes = changes.soft_merge_changes(local_changes, exterior_changes)  # merge local and exterior changes
+            tfile.changes[local_m] = merged_local_changes
+            changes.make_changes(staged_changes, tfile.tpath, tfile.mpath[local_m])  # apply staged changes
+            tfile.changes[exterior_m] = changes.empty_changes  # clear exterior changes (applied)
+
+        #display#
         print('APPLY')
         print('local changes:')
-        print('exterior changes:')
+        for change in local_changes:
+            change.display()
         print('')
-
-        if (not changes.is_empty(tfile.changes[local_m])) and (not changes.is_empty(tfile.changes[exterior_m])):  # if there are changes on the local and exterior machine
-            tfile.changes[local_m], staged_changes = changes.soft_merge_changes(tfile.changes[local_m], tfile.changes[exterior_m])  # merge local and exterior changes
-
+        print('exterior changes:')
+        for change in exterior_changes:
+            change.display()
+        print('')
+        if (not changes.is_empty(local_changes)) and (
+        not changes.is_empty(exterior_changes)):  # if there are changes on the local and exterior machine
             print('merged local changes:')
-            print('staged changes:')
+            for change in merged_local_changes:
+                change.display()
             print('')
-
-            changes.make_changes(staged_changes, tfile.tpath, tfile.mpath[local_m])  # apply staged changes
+            print('staged changes:')
+            for change in staged_changes:
+                change.display()
+            print('')
             print('staged changes applied')
-            tfile.changes[exterior_m] = changes.empty_changes  # clear exterior changes (applied)
         else:
             print('no staged changes')
 
-        #download#
+        #DOWNLOAD#
         tfile.download()
 
     #title#
